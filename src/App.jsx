4c8c30bd05ec32b79de75183412fb328de858606
Magno <jmagno.dutra@hotmail.com>
2025-01-19 02:32:09 -0300

    73 - first steps towards our game

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 function App() {
-  return <p>comming soon</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
